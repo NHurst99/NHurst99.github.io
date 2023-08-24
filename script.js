bcdebc1593fd7873223d1a49d2dee25b7b59d110
Nick <cooltop101@gmail.com>
2023-08-23 17:40:32 -0700

    Added link list and pin lock

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('contactCard').addEventListener('click', function() {
+  const pinPopup = document.getElementById('pinPopup');
+  pinPopup.style.display = 'block';
+});
+
+document.getElementById('submitPin').addEventListener('click', function() {
+  const pin = Array.from(document.querySelectorAll('.pin-digit')).map(input => input.value).join('');
+  fetch('https://your-project.vercel.app/api/auth', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ pin })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.valid) {
+      window.location.href = data.url; // Redirect to the download link
+    } else {
+      alert('Incorrect PIN. Please try again.');
+    }
+  });
+});
